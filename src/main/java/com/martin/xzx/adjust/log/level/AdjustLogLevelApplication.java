e50046ffe47d26f69bf7081eeb3dbd76ab6449e1
xuzongxin <xuzongxin@jd.com>
2021-11-29 20:10:13 +0800

    代码初始化

---
+++ src/main/java/com/martin/xzx/adjust/log/level/AdjustLogLevelApplication.java
@@ -0,0 +1,13 @@
+package com.martin.xzx.adjust.log.level;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdjustLogLevelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdjustLogLevelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/martin/xzx/adjust/log/level/AdjustLogLevelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.martin.xzx.adjust.log.level;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdjustLogLevelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
